ac58ae5f7168852de26c2aa5cb5784b75d505eab
luis fernando <fer.almeidabr@hotmail.com>
2021-10-22 12:09:13 -0300

    Primeira sessão de MySQL

---
+++ Aula 01 Banco de dados.sql
@@ -0,0 +1,25 @@
+create database db_acervo35;
+drop database db_acervo35;
+
+use db_acervo35;
+create table tb_usuario(
+	id_usuario bigint auto_increment,
+	nome varchar(255) not null,
+    email varchar(255) not null,
+    nascimento_data date,
+    senha varchar(255) not null,
+    
+    primary key (id_usuario)
+);
+
+select * from db_acervo35.tb_usuario;
+
+insert into tb_usuario (nome, email, nascimento_data, senha)
+	values ("Lissa", "email@email.com", '1995-01-30', "123456");
+    
+delete from tb_usuario where id_usuario = 1;
+
+update `db_acervo35`.`tb_usuario` set `senha` = 333 where (`id_usuario` = '2');
+
+alter table tb_usuario add livro varchar(30);
+	
